913f16e0f212000223faab90f452ddbd80cb11d1
imvmanish19 <imvmanish18@gmail.com>
2021-05-25 11:22:12 +0530

    made api calls to weather api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template ,request
+from flask import jsonify, make_response
 import joblib
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -12,8 +14,12 @@ def hello_world():
 def crop():
     return render_template('crop.html')
 
-def weather_api():
-    
+def weather_api(city_name):
+    API_key = '2eb7f9289b3e6ee8b9d43ec77776e0fa'
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}")
+    return response.json()
+
+# def kelvin_to_celcius():
 
 @app.route('/prediction',methods=["POST"])
 def predict_crop():
@@ -21,8 +27,12 @@ def predict_crop():
     place = request.form['place']
     rainfall = request.form['rainfall']
     ph = request.form['ph']
-    prediction = model.predict([[20,82,6.57,92]])
-    return str(prediction)
+    output = weather_api(place)
+    temperature = output['main']['temp']
+    humidity = output['main']['humidity']
+    return output 
+    # prediction = model.predict([[20,82,6.57,92]])
+    # return str(prediction)
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
