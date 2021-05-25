559f2994b0d0fec732349be3c9b1d133e272fcd0
imvmanish19 <imvmanish18@gmail.com>
2021-05-25 11:37:32 +0530

    crop prediction part done

--- app.py
+++ app.py
@@ -19,7 +19,46 @@ def weather_api(city_name):
     response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}")
     return response.json()
 
-# def kelvin_to_celcius():
+def kelvin_to_celsius(kelvin):
+    celsius = float(kelvin) - 273.15
+    return celsius
+
+def crop_name(crop_number):
+    dict = {
+    1: 'Ground Nut',
+    2: 'Chickpea',
+    3: 'Peas',
+    4: 'watermelon',
+    5: 'apple',
+    6: 'banana',
+    7: 'papaya',
+    8: 'millet',
+    9: 'Lentil',
+    10: 'Adzuki Beans',
+    11: 'rice',
+    12: 'maize',
+    13: 'wheat',
+    14: 'Mung Bean',
+    15: 'Moth Beans',
+    16: 'Black gram',
+    17: 'Jute',
+    18: 'Sugarcane',
+    19: 'orange',
+    20: 'Rubber',
+    21: 'Kidney Beans',
+    22: 'pomegranate',
+    23: 'grapes',
+    24: 'Tobacco',
+    25: 'Pigeon Peas',
+    26: 'Tea',
+    27: 'Coffee',
+    28: 'Cotton',
+    29: 'muskmelon',
+    30: 'mango',
+    31: 'Coconut'
+    }
+    answer = dict[crop_number]
+    return answer
 
 @app.route('/prediction',methods=["POST"])
 def predict_crop():
@@ -29,10 +68,12 @@ def predict_crop():
     ph = request.form['ph']
     output = weather_api(place)
     temperature = output['main']['temp']
-    humidity = output['main']['humidity']
-    return output 
-    # prediction = model.predict([[20,82,6.57,92]])
-    # return str(prediction)
+    humidity = float(output['main']['humidity'])
+    temp = kelvin_to_celsius(temperature)
+    prediction = model.predict([[temp,humidity,ph,rainfall]])
+    prediction = int(prediction)
+    crop_name_predicted = crop_name(prediction)
+    return crop_name_predicted
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
